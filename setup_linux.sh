f7f3ef1efd334109ada47efe6131a70d4ad82a5e
osellers <olliejsellers@gmail.com>
2025-03-03 15:07:29 +0000

    init

---
+++ setup_linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install fish
+sudo apt-get install fish
+
+# set as default shell
+fish_path=$(which fish)
+chsh -s "$fish_path"
+
+# configure fish
+fish -c conda init fish
+fish -c fish_add_path /opt/homebrew/bin
+
+# setup nvim
+sudo apt-get install neovim
+rm -rf ~/.config/nvim
+cp -r ./nvim ~/.config/
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+# install python language server
+pip install -U jedi-language-server
---
+++ setup_mac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# install fish
+brew install fish
+
+# set as default shell
+fish_path=$(which fish)
+chsh -s "$fish_path"
+
+# configure fish
+fish -c conda init fish
+fish -c fish_add_path /opt/homebrew/bin
+
+# setup nvim
+brew install nvim
+cp -r ./nvim ~/.config/
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+# install python language server
+pip install -U jedi-language-server
